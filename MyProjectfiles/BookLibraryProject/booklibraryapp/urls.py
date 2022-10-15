ba2dd8f4f69ac07bcee4aa1bdde26d2753a15f40
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-10-15 00:38:54 -0400

    nav bar edits and more html template

--- MyProjectfiles/BookLibraryProject/booklibraryapp/urls.py
+++ MyProjectfiles/BookLibraryProject/booklibraryapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='booklibrary'),
+    path('toreadlist', views.toReadList, name='toreadlist'),
+    path('findabook', views.findaBook, name='findabook')
 ]
--- MyProjectfiles/BookLibraryProject/booklibraryapp/views.py
+++ MyProjectfiles/BookLibraryProject/booklibraryapp/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "booklibrary/index.html")
+
+def toReadList(request):
+    return render(request, "booklibrary/toReadList.html")
+
+def findaBook(request):
+    return render(request, "booklibrary/findaBook.html")
